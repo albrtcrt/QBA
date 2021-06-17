c724e18028ea1cf536333bec45d2359b2caa9995
albrtcrt <85366724+albrtcrt@users.noreply.github.com>
2021-06-17 18:01:52 +0200

    Update QueueDBA_Tester.c

--- QueueDBA_Tester.c
+++ QueueDBA_Tester.c
@@ -37,37 +37,38 @@ int main(int argc, char const **argv) {
 	QueueDBA coda = {0};
 	if(argc < 3) {
 		printf("Numero file di input errato.");
-        exit(0);
+		exit(0);
 	} /* if */
 	strcpy(nameFileIn, argv[1]);
 	strcpy(nameFileOut, argv[2]);
 	inF = fopen(nameFileIn, "r");
 	if (inF == NULL)
-        return -1;							/* apertura fallita di $nameFileIn */
+		return -1;											/* apertura fallita di $nameFileIn */
 	outF = fopen(nameFileOut, "w");
-    if (outF == NULL) {
-        fclose(inF);
-        return -2;							/* apertura fallita di $nameFileOut */
-    } /* if */
+	if (outF == NULL) {
+		fclose(inF);
+		return -2;											/* apertura fallita di $nameFileOut */
+	} /* if */
 	
 	while(!feof(inF)) {
 		fscanf(inF, "%s", read);
-        if(strcmp(read ,"IN") == 0) {		/* queueInit */
+		
+		if(strcmp(read ,"IN") == 0) {						/* queueInit */
 			queueInit(&coda);
 			fprintf(outF, "%s [ ]\n", read);
 		} /* if */
 		
-		else if(strcmp(read ,"CA") == 0) {	/* queueCapacity */
+		else if(strcmp(read ,"CA") == 0) {					/* queueCapacity */
 			fprintf(outF, "CA %d ", queueCapacity(&coda));
 			printQueue(&coda, outF);
 		} /* else if */
 		
-		else if(strcmp(read ,"SI") == 0) {	/* queueSize */
+		else if(strcmp(read ,"SI") == 0) {					/* queueSize */
 			fprintf(outF, "SI %d ", queueSize(&coda));
 			printQueue(&coda, outF);
 		} /* else if */
 		
-		else if(strcmp(read ,"IE") == 0) {	/* queueIsEmpty */
+		else if(strcmp(read ,"IE") == 0) {					/* queueIsEmpty */
 			if (queueIsEmpty(&coda))
 				fprintf(outF, "IE true ");
 			else
@@ -75,15 +76,15 @@ int main(int argc, char const **argv) {
 			printQueue(&coda, outF);
 		} /* else if */
 		
-		else if(strcmp(read ,"AL") == 0) {	/* queueAddLast */
+		else if(strcmp(read ,"AL") == 0) {					/* queueAddLast */
 			fscanf(inF, "%4s", read);
 			temp = atof(read);
-			queueAddLast(&coda, temp);			
+			queueAddLast(&coda, temp);
 			fprintf(outF, "AL %.1f ", temp);
 			printQueue(&coda, outF);
 		} /* else if */
 		
-		else if(strcmp(read ,"GF") == 0) {	/* queueGetFirst */
+		else if(strcmp(read ,"GF") == 0) {					/* queueGetFirst */
 			temp = queueGetFirst(&coda);
 			if(temp == -1) {
 				fprintf(outF, "GF KO ");
@@ -94,10 +95,11 @@ int main(int argc, char const **argv) {
 			}
 		} /* else if */
 		
-		else if(strcmp(read ,"RF") == 0) {	/* queueRemoveFirst */
+		else if(strcmp(read ,"RF") == 0) {					/* queueRemoveFirst */
 			temp = queueRemoveFirst(&coda);
 			if(temp == -1) {
 				fprintf(outF, "RF KO ");
+				checkAndSet(&coda);
 				printQueue(&coda, outF);
 			} else {
 				fprintf(outF, "RF %.1f ", temp);
@@ -105,14 +107,14 @@ int main(int argc, char const **argv) {
 			}
 		} /* else if */
 		
-		else if(strcmp(read ,"FR") == 0) {	/* queueFree */
+		else if(strcmp(read ,"FR") == 0) {					/* queueFree */
 			fprintf(outF, "FR\n");
 			queueFree(&coda);
 		} /* else if */
 	} /* while */
 	
 	fclose(inF);
-    fclose(outF);
+	fclose(outF);
 	queueFree(&coda);
 	return 0;
 } /* main */
